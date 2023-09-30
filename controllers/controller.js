8c15d2121246a1946677d0249e1df065f5eaa899
Guptaverse <sggreat88@gmail.com>
2023-09-30 19:59:04 +0530

    update

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,11 +1,10 @@
-const _ = require("lodash");
+const lodash = require("lodash");
 const util = require("util");
 const exec = util.promisify(require("child_process").exec);
 
-// Use Lodash's memoize function to cache the fetchData function with a caching period of 5 minutes (300,000 milliseconds)
-const memoizedFetchData = _.memoize(fetchData, undefined, 300000);
+const memoizedFetchData = lodash.memoize(fetchData, undefined, 300000);
 
-// Define a function to fetch data asynchronously
+
 async function fetchData() {
   const curlCommand = `curl -s --request GET \
     --url https://intent-kit-16.hasura.app/api/rest/blogs \
@@ -30,13 +29,13 @@ exports.blogStats = async (req, res) => {
 
     // Data Analysis
     const totalBlogs = blogs.length;
-    const longestBlog = _.maxBy(blogs, "title.length");
+    const longestBlog = lodash.maxBy(blogs, "title.length");
     const blogsWithPrivacy = blogs.filter((blog) =>
       blog.title.toLowerCase().includes("privacy")
     );
-    const uniqueBlogTitles = _.uniqBy(blogs, "title");
+    const uniqueBlogTitles = lodash.uniqBy(blogs, "title");
 
-    // Respond with JSON object containing statistics
+    // Response-> JSON object containing statistics
     res.json({
       totalBlogs,
       longestBlog: longestBlog.title,
@@ -51,8 +50,6 @@ exports.blogStats = async (req, res) => {
 
 exports.blogSearch = async (req, res) => {
   let query = req.query.query;
-
-  // Ensure query parameter is provided
   if (!query) {
     return res
       .status(400)
